89f58014c2386fb8caee00976ab18a0e2b56a209
Yohaan <yozaam@gmail.com>
2020-12-02 05:51:23 +0000

    hello world in java

--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 class Main {
+
+  // first basic program: 
+  public static void helloUser() {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Who is it?");
+    String name = sc.nextLine();
+    System.out.println("Hello " + name);
+  }
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+
+    
   }
 }
